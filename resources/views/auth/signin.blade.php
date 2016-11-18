7167fc6dc757333e196900b4b6f3877aed4b4649
prolin99 <prolin99@gmail.com>
2016-11-18 08:29:01 +0800

    bootstrap3 ,驗證檢查

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -1,16 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
-    <form action="{{ '/signin' }}" method="post">
-        <div class="input-group">
-            <label for="email">E-Mail</label>
-            <input type="email" id="email" name="email">
+    <h2>Login</h2>
+    <form class="form-horizontal" action="{{ url('/signin') }}" method="post">
+        <div class="form-group">
+          <label for="email" class="col-sm-4 control-label">Email</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Email">
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+          </div>
         </div>
-        <div class="input-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
+        <div class="form-group">
+          <label for="password" class="col-sm-4 control-label">Password</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+          </div>
         </div>
+
         {{ csrf_field() }}
-        <button type="submit">Sign In</button>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Sign in</button>
+          </div>
+        </div>
+
     </form>
 @endsection
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,24 +1,60 @@
 @extends('layouts.master')
 
 @section('content')
-    <form action="{{ '/signup' }}" method="post">
-        <div class="input-group">
-            <label for="first_name">First Name</label>
-            <input type="text" id="first_name" name="first_name">
+    <h2>Register Form</h2>
+    <form class="form-horizontal" action="{{ url('/signup') }}" method="post">
+        <div class="form-group">
+          <label for="first_name" class="col-sm-4 control-label">First Name</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="first_name" name="first_name" value="{{ old('first_name') }}" placeholder="First Name">
+            @if ($errors->has('first_name'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('first_name') }}</strong>
+                </span>
+            @endif
+          </div>
         </div>
-        <div class="input-group">
-            <label for="last_name">Last Name</label>
-            <input type="text" id="last_name" name="last_name">
+        <div class="form-group">
+          <label for="last_name" class="col-sm-4 control-label">Last Name</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="last_name" name="last_name" value="{{ old('last_name') }}" placeholder="Last Name">
+            @if ($errors->has('last_name'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('last_name') }}</strong>
+                </span>
+            @endif
+          </div>
         </div>
-        <div class="input-group">
-            <label for="email">E-Mail</label>
-            <input type="email" id="email" name="email">
+
+        <div class="form-group">
+          <label for="email" class="col-sm-4 control-label">E-Mail</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}"  placeholder="Email">
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+          </div>
         </div>
-        <div class="input-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
+        <div class="form-group">
+          <label for="password" class="col-sm-4 control-label">Password</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password  at least 6 characters">
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+          </div>
         </div>
+
         {{ csrf_field() }}
-        <button type="submit">Sign Up</button>
+
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-success">Sign Up</button>
+          </div>
+        </div>
     </form>
 @endsection
